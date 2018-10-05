ff9ab3919cea74e7ce8fc069eacb2ab8a824987a
marisb <maris.bergs@isoft.lv>
2018-10-05 10:16:15 -0700

    Sākotnējā versija
    Sākotnējā versija

---
+++ src/ControlitFactory/Theming/ColorPalette.xaml.cs
@@ -0,0 +1,12 @@
+using Xamarin.Forms;
+
+namespace ControlitFactory.Theming
+{
+    public partial class ColorPalette : ResourceDictionary
+    {
+        public ColorPalette()
+        {
+            InitializeComponent();
+        }
+    }
+}
